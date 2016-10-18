46ee8540a8c2d1007fb608d43d7f74a8e34061eb
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-10-18 13:28:59 +0200

    inital commit

---
+++ bootstrap_neo.py
@@ -0,0 +1,6 @@
+from neo4j.v1 import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("",""), encrypted=False)
+session = driver.session()
+
+session.run("CREATE CONSTRAINT ON (f:File) ASSERT f.path IS UNIQUE;")
---
+++ repo_contents.py
@@ -0,0 +1,44 @@
+from neo4j.v1 import GraphDatabase, basic_auth
+import requests
+import json
+
+driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("",""), encrypted=False)
+session = driver.session()
+
+repository = "PrestaShop/PrestaShop"
+contents_url = "https://api.github.com/repos/" + repository + "/contents/"
+
+def getDirectoryContent(path):
+    url = contents_url + path
+    response = requests.get(url)
+
+    return response.text
+
+def traverse(currentDir):
+    files = []
+    contents = json.loads(getDirectoryContent(currentDir+"?client_id=" + client_id + "&client_secret=" + client_secret))
+    for elt in contents:
+        if elt["type"] == "dir":
+            traverse(elt["path"])
+        elif elt["type"] == "file":
+          files.append(elt["path"])
+
+    q = '''UNWIND {files} AS file
+            WITH split(file,"/") AS elts
+            WITH elts WHERE size(elts) > 1
+            UNWIND range(0,size(elts)-1) AS i
+            MERGE (prev:File {path: reduce(s="",x IN range(0,i-2) | s+ elts[x]+"/") + CASE i WHEN 0 THEN "" ELSE elts[i-1] END})
+            SET prev.name = CASE i WHEN 0 THEN "." ELSE elts[i-1] END
+            MERGE (file:File {path: reduce(s="",x IN range(0,i-1) | s+ elts[x]+"/") + elts[i]})
+            SET file.name = elts[i]
+            MERGE (file)-[:PARENT]->(prev)
+            WITH reduce(s="",x IN range(0,size(elts)-2) | s+ elts[x]+"/") + elts[-1] AS la
+            MATCH (f:File {path:la})
+            SET f:FileRecord'''
+    session.run(q, {"files": files})
+    print(currentDir)
+
+
+currentDir = "."
+traverse(currentDir)
+
